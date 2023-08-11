3349c3c8ff2b02113b083728e11838f47debfe27
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-08-10 23:17:36 -0500

    endpoing method get route api/genders

---
+++ EFCoreFilms/Controllers/GendersController.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreFilms.entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreFilms.Controllers
+{
+    [ApiController]
+    [Route("api/genders")]
+    public class GendersController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GendersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Gender>> Get()
+        {
+            return await context.Genders.ToListAsync();
+        }
+    }
+}
