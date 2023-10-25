cd8fae6bb18ed4c5c50b43f005cfd3a3e1579672
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-10-24 20:51:41 -0500

    Select with anonymous

---
+++ EFCoreFilms/Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using EFCoreFilms.entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreFilms.Controllers
+{
+    [ApiController]
+    [Route("api/actors")]
+    public class ActorsController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActorsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var actors = await context.Actors.Select(a => new {Id = a.Id, Name = a.Name}).ToListAsync();
+            return Ok(actors);
+        }
+    }
+}
