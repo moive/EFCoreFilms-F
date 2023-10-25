654285809a725de0fb5525b7aabef5d6cf6552d3
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-10-24 22:02:00 -0500

    Distance location nearby

--- EFCoreFilms/Controllers/CinemasController.cs
+++ EFCoreFilms/Controllers/CinemasController.cs
@@ -4,6 +4,8 @@ using EFCoreFilms.DTOs;
 using EFCoreFilms.entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
 
 namespace EFCoreFilms.Controllers
 {
@@ -24,6 +26,26 @@ namespace EFCoreFilms.Controllers
         public async Task<IEnumerable<CinemaDTO>> Get()
         {
             return await context.Cinemas.ProjectTo<CinemaDTO>(mapper.ConfigurationProvider).ToListAsync();
+        }
+
+        [HttpGet("nearby")]
+        public async Task<ActionResult> Get(double latitude, double longitude)
+        {
+            //lat: 18.483272, lng: -69.940143 // lat:18.469220 , lng: -69.939895
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+            var myLocation = geometryFactory.CreatePoint(new Coordinate(longitude, latitude));
+            var maximumDistanceInMeters = 2000;
+
+            var cinema = await context.Cinemas
+                .OrderBy(c => c.Location.Distance(myLocation))
+                .Where(x => x.Location.IsWithinDistance(myLocation, maximumDistanceInMeters))
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Distance = Math.Round(c.Location.Distance(myLocation))
+                }).ToListAsync();
+
+            return Ok(cinema);
         }
     }
 }
