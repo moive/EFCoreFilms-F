14536624ae3c2d6a8e83311174d53c1ebb9a8a3d
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-10-25 22:57:29 -0500

    Eagle loading - Include data relational

---
+++ EFCoreFilms/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using EFCoreFilms.entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreFilms.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Films>> Get(int id)
+        {
+            var movie = await context.Films
+                .Include(x => x.Genders)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie is null) { return NotFound(); }
+
+            return movie;
+        }
+    }
+}
