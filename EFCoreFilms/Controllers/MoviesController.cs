ca6b73e9a9903efb5bdfc61574478ff2ebe9f867
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-10-29 23:47:58 -0500

    explicit loading

--- EFCoreFilms/Controllers/MoviesController.cs
+++ EFCoreFilms/Controllers/MoviesController.cs
@@ -67,5 +67,23 @@ namespace EFCoreFilms.Controllers
             if (movie is null) { return NotFound(); }
             return Ok(movie);
         }
+
+        //disadvantage: not very efficient
+        [HttpGet("explicitLoading/{id:int}")]
+        public async Task<ActionResult<MovieDTO>> GetExplicit(int id)
+        {
+            var movie = await context.Films.AsTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            await context.Entry(movie).Collection(p => p.Genders).LoadAsync();
+
+            var genderCount = await context.Entry(movie).Collection(p => p.Genders).Query().CountAsync();
+
+            if (movie is null) { return NotFound(); };
+
+            var movieDTO = mapper.Map<MovieDTO>(movie);
+
+            return movieDTO;
+        }
     }
 }
