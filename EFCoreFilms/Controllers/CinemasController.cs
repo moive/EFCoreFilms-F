8809bca0177dfef9c88a464cd764dc6cde7afa81
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-02-21 19:18:49 -0500

    Insert records with new related data

--- EFCoreFilms/Controllers/CinemasController.cs
+++ EFCoreFilms/Controllers/CinemasController.cs
@@ -47,5 +47,41 @@ namespace EFCoreFilms.Controllers
 
             return Ok(cinema);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+            var myLocation = geometryFactory.CreatePoint(new Coordinate(-69.896979, 18.476276));
+
+            var cinema = new Cinema()
+            {
+                Name = "My Excelsius",
+                Location = myLocation,
+                CinemaOffer = new CinemaOffer()
+                {
+                    DiscountPercentage = 5,
+                    StartDate = DateTime.Today,
+                    EndDate = DateTime.Today.AddDays(7)
+                },
+                Cinemaroom = new HashSet<CinemaRoom>()
+                {
+                    new CinemaRoom()
+                    {
+                        Price = 200,
+                        CinemaType = CinemaType.TwoDimensions
+                    },
+                    new CinemaRoom()
+                    {
+                        Price = 300,
+                        CinemaType = CinemaType.ThreeDimensions
+                    }
+                }
+            };
+
+            context.Add(cinema);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
